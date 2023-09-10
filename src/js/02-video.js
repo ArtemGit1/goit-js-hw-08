7fe283e211ccc7ef1e1e526866a79e0f4cb7c0be
ArtemGit1 <131163813+ArtemGit1@users.noreply.github.com>
2023-09-10 18:58:57 +0300

    123

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+const iframe = document.getElementById('vimeo-player');
+const player = new Vimeo.Player(iframe);
 
+
+function saveCurrentTime(time) {
+  localStorage.setItem('videoplayer-current-time', time);
+}
+
+
+player.on('timeupdate', _.throttle(function(event) {
+  const currentTime = Math.floor(event.seconds);
+  saveCurrentTime(currentTime);
+}, 1000));
+
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
+
+
+player.play();
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,51 @@
+import throttle from 'lodash.throttle';
+
+
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+
+const saveFormDataToLocalStorage = () => {
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+};
+
+
+const populateFormFieldsFromLocalStorage = () => {
+  const savedFormData = localStorage.getItem('feedback-form-state');
+  if (savedFormData) {
+    const formData = JSON.parse(savedFormData);
+    emailInput.value = formData.email;
+    messageInput.value = formData.message;
+  }
+};
+
+
+const throttledSaveFormData = throttle(saveFormDataToLocalStorage, 500);
+
+emailInput.addEventListener('input', throttledSaveFormData);
+messageInput.addEventListener('input', throttledSaveFormData);
+
+
+window.addEventListener('load', populateFormFieldsFromLocalStorage);
+
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageInput.value = '';
+
+
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  console.log(formData);
+});
 
