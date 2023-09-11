b0cc8b57cce331ad6397f4c347e827506c4b2bcf
ArtemGit1 <131163813+ArtemGit1@users.noreply.github.com>
2023-09-11 15:35:12 +0300

    123

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,22 +2,24 @@ import Player from '@vimeo/player';
 const iframe = document.getElementById('vimeo-player');
 const player = new Vimeo.Player(iframe);
 
-
 function saveCurrentTime(time) {
   localStorage.setItem('videoplayer-current-time', time);
 }
 
+document.getElementById('startVideoButton').addEventListener('click', () => {
+  player.play().catch((error) => {
+
+    console.error('Помилка відтворення відео:', error);
+  });
+});
 
 player.on('timeupdate', _.throttle(function(event) {
   const currentTime = Math.floor(event.seconds);
   saveCurrentTime(currentTime);
 }, 1000));
 
-
 const savedTime = localStorage.getItem('videoplayer-current-time');
 if (savedTime) {
   player.setCurrentTime(savedTime);
 }
 
-
-player.play();
